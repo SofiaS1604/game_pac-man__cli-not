8e8e3fb8a5a13d00c22cd17af70715a7762209f3
SofiaS1604 <SofiaS1604@yandex.ru>
2019-01-26 12:25:58 +0300

    Add files via upload

--- game_cli_not/script/index.js
+++ game_cli_not/script/index.js
@@ -54,15 +54,11 @@ let game = new Vue({
     },
 
 
-
-
     gameOver() {
       this.game_start = true;
       this.game_play = false;
       this.game_time = 100
     },
-
-
 
 
     level_info: function(id){
@@ -72,26 +68,16 @@ let game = new Vue({
       this.game_level = false;
       this.game_play = true;
 
-      for (let i = 0; i < 45 ; i++) {
+      for (let i = 0; i < 55 ; i++) {
         let wall = this.walls.push(i);
 
         let left = Math.floor(Math.floor(Math.random() * (36/6)) + 1);
         let top = Math.floor(Math.floor(Math.random() * (46/5)) + 1);
 
-
         let wall_top = this.walls_top.push(((Math.floor(Math.random() * (top)) + 1)*60)-15);
         let wall_left = this.walls_left.push(((Math.floor(Math.random() * (left)) + 1)*105)-60);
 
-
-
-
-
-
-
       }
-
-
-
     },
 
 
@@ -102,7 +88,7 @@ let game = new Vue({
           let indentification = 0;
 
           for(let i = 0; i < this.walls.length; i++ ){
-              if((this.walls_top[i] === this.circle_y*15 || this.walls_top[i]+15 === this.circle_y*15) && this.walls_left[i]+30 === this.circle_x*15  ){
+              if((this.walls_top[i] === this.circle_y*15 || this.walls_top[i]+15 === this.circle_y*15) && this.walls_left[i]+30 === this.circle_x*15){
                   indentification = 1;
               }
           }
@@ -118,7 +104,7 @@ let game = new Vue({
           let indentification = 0;
 
           for(let i = 0; i < this.walls_left.length+1; i++ ){
-              if(this.walls_top[i]+30 === this.circle_y*15 && (this.walls_left[i] === this.circle_x*15 || this.walls_left[i]+15 === this.circle_x*15) ){
+              if(this.walls_top[i]+30 === this.circle_y*15 && (this.walls_left[i] === this.circle_x*15 || this.walls_left[i]+15 === this.circle_x*15)){
                   indentification = 1;
               }
           }
@@ -130,32 +116,31 @@ let game = new Vue({
     },
 
     right(){
-      if(this.circle_x < this.cell - 1){
+        if(this.circle_x < this.cell - 1){
 
-          let indentification = 0;
+            let indentification = 0;
 
-          for(let i = 0; i < this.walls.length; i++ ){
-              if((this.walls_top[i] === this.circle_y*15 || this.walls_top[i]+15 === this.circle_y*15) && this.walls_left[i]-15 === this.circle_x*15  ){
-                  indentification = 1;
-              }
-          }
+            for(let i = 0; i < this.walls.length; i++ ){
+                if((this.walls_top[i] === this.circle_y*15 || this.walls_top[i]+15 === this.circle_y*15) && this.walls_left[i]-15 === this.circle_x*15  ) {
+                    indentification = 1;
+                }
+            }
 
-          if(indentification !== 1){
-              this.circle_x ++;
-          }
-
-      }
+            if(indentification !== 1){
+                this.circle_x ++;
+            }
+        }
     },
 
     bottom(){
-      if(this.circle_y < this.row -1){
-          let indentification = 0;
+        if(this.circle_y < this.row -1){
+            let indentification = 0;
 
-          for(let i = 0; i < this.walls.length; i++ ){
-              if(this.walls_top[i]-15 === this.circle_y*15 && (this.walls_left[i] === this.circle_x*15 || this.walls_left[i]+15 === this.circle_x*15) ){
+            for(let i = 0; i < this.walls.length; i++ ){
+                if(this.walls_top[i]-15 === this.circle_y*15 && (this.walls_left[i] === this.circle_x*15 || this.walls_left[i]+15 === this.circle_x*15)) {
                   indentification = 1;
-              }
-          }
+                }
+            }
 
           if(indentification !== 1){
                 this.circle_y ++;
@@ -166,20 +151,20 @@ let game = new Vue({
 
 
     run: function (e) {
-      switch (e.keyCode) {
-        case 37:
-          this.left();
-          break;
-        case 38:
-          this.top();
-          break;
-        case 39:
-          this.right();
-          break;
-        case 40:
-          this.bottom()
-
-      }
+        switch (e.keyCode) {
+            case 37:
+                this.left();
+                break;
+            case 38:
+                this.top();
+                break;
+            case 39:
+                this.right();
+                break;
+            case 40:
+                this.bottom();
+                break;
+        }
     }
 
 
