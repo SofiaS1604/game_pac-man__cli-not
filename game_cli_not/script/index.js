89dff1898040b2001bd08af02df2932456139351
SofiaS1604 <SofiaS1604@yandex.ru>
2019-01-27 00:47:11 +0300

    Add files via upload

--- game_cli_not/script/index.js
+++ game_cli_not/script/index.js
@@ -1,182 +1,180 @@
 let game = new Vue({
-  el: '.game',
-  data:{
-    game_start: true,
-    game_level: false,
-    game_play: false,
-    game_time: 100,
-    min : null,
-    sec: null,
-    levels: [],
-    walls: [],
-    walls_left: [],
-    walls_top: [],
-    row: 36,
-    cell: 46,
-    circle_x: 0,
-    circle_y: 0,
-  },
-
-  methods: {
-    button_play: function () {
-      this.game_start = false;
-      this.game_level = true;
-
-
-      for (let i = 1; i < 21; i++) {
-        let level = this.levels.push(i);
-      }
-
+    el: '.game',
+    data:{
+        game_start: true,
+        game_level: false,
+        game_play: false,
+        game_time: 90,
+        min : null,
+        sec: null,
+        levels: [],
+        walls: [],
+        walls_left: [],
+        walls_top: [],
+        row: 36,
+        cell: 46,
+        circle_x: 0,
+        circle_y: 0,
+        target: null
     },
 
-    gameTimer() {
-      if (this.game_time > 0) {
-        this.game_time--;
-        this.min =  Math.floor(this.game_time / 60);
-        this.sec = this.game_time - ((Math.floor(this.game_time / 60)) * 60);
+    methods: {
+        button_play: function () {
+            this.game_start = false;
+            this.game_level = true;
+            this.game_play = false;
+            this.levels = [];
+            this.walls = [];
+            this.walls_left = [];
+            this.walls_top = [];
+            this.circle_x = 0;
+            this.circle_y = 0;
 
-        if(this.min < 10 ){
-          this.min = '0' + this.min;
-        }
+            for (let i = 1; i < 21; i++) {
+                this.levels.push(i);
+            }
+        },
 
-        if (this.sec < 10) {
-          this.sec = '0' + this.sec;
-        }
+        gameTimer() {
+            if (this.game_time > 0) {
 
+                this.game_time--;
+                this.min =  Math.floor(this.game_time / 60);
+                this.sec = this.game_time - ((Math.floor(this.game_time / 60)) * 60);
 
-        setTimeout(() => {
-          this.gameTimer();
-        }, 1000);
-      } else {
-        this.gameOver();
-      }
+                if(this.min < 10 ){
+                    this.min = '0' + this.min;
+                }
 
-    },
+                if (this.sec < 10) {
+                    this.sec = '0' + this.sec;
+                }
 
+                setTimeout(() => {
+                    this.gameTimer();
+                }, 1000);
 
-    gameOver() {
-      this.game_start = true;
-      this.game_play = false;
-      this.game_time = 100
-    },
+            }else {
+                this.gameOver();
+            }
+        },
 
-
-    level_info: function(id){
-
-      this.gameTimer();
-
-      this.game_level = false;
-      this.game_play = true;
-
-      for (let i = 0; i < 55 ; i++) {
-        let wall = this.walls.push(i);
-
-        let left = Math.floor(Math.floor(Math.random() * (36/6)) + 1);
-        let top = Math.floor(Math.floor(Math.random() * (46/5)) + 1);
-
-        let wall_top = this.walls_top.push(((Math.floor(Math.random() * (top)) + 1)*60)-15);
-        let wall_left = this.walls_left.push(((Math.floor(Math.random() * (left)) + 1)*105)-60);
-
-      }
-    },
+        gameOver() {
+            this.game_start = true;
+            this.game_play = false;
+            this.game_level = false;
+            this.game_time = 90;
+        },
 
 
+        level_info: function(id){
+
+            this.target = id+35;
+            this.gameTimer();
+            this.game_level = false;
+            this.game_play = true;
+            this.game_start = false;
+
+            for (let i = 0; i < 55 ; i++) {
+                this.walls.push(i);
+
+                let left = Math.floor(Math.floor(Math.random() * (36/6)) + 1);
+                let top = Math.floor(Math.floor(Math.random() * (46/5)) + 1);
+
+                this.walls_top.push(((Math.floor(Math.random() * (top)) + 1)*60)-15);
+                this.walls_left.push(((Math.floor(Math.random() * (left)) + 1)*105)-60);
+            }
+        },
 
 
-    left(){
-      if(this.circle_x > 0){
-          let indentification = 0;
 
-          for(let i = 0; i < this.walls.length; i++ ){
-              if((this.walls_top[i] === this.circle_y*15 || this.walls_top[i]+15 === this.circle_y*15) && this.walls_left[i]+30 === this.circle_x*15){
-                  indentification = 1;
-              }
-          }
 
-          if(indentification !== 1){
-              this.circle_x --;
-          }
-      }
-    },
+        left(){
+            if(this.circle_x > 0){
+                let indentification = 0;
 
-    top(){
-      if(this.circle_y > 0){
-          let indentification = 0;
+                for(let i = 0; i < this.walls.length; i++ ){
+                    if((this.walls_top[i] === this.circle_y*15 || this.walls_top[i]+15 === this.circle_y*15) && this.walls_left[i]+30 === this.circle_x*15){
+                        indentification = 1;
+                    }
+                }
 
-          for(let i = 0; i < this.walls_left.length+1; i++ ){
-              if(this.walls_top[i]+30 === this.circle_y*15 && (this.walls_left[i] === this.circle_x*15 || this.walls_left[i]+15 === this.circle_x*15)){
-                  indentification = 1;
-              }
-          }
-
-          if(indentification !== 1){
-              this.circle_y --;
-          }
-      }
-    },
-
-    right(){
-        if(this.circle_x < this.cell - 1){
-
-            let indentification = 0;
-
-            for(let i = 0; i < this.walls.length; i++ ){
-                if((this.walls_top[i] === this.circle_y*15 || this.walls_top[i]+15 === this.circle_y*15) && this.walls_left[i]-15 === this.circle_x*15  ) {
-                    indentification = 1;
+                if(indentification !== 1){
+                    this.circle_x --;
                 }
             }
+        },
 
-            if(indentification !== 1){
-                this.circle_x ++;
-            }
-        }
-    },
+        top(){
+            if(this.circle_y > 0){
+                let indentification = 0;
 
-    bottom(){
-        if(this.circle_y < this.row -1){
-            let indentification = 0;
+                for(let i = 0; i < this.walls_left.length+1; i++ ){
+                    if(this.walls_top[i]+30 === this.circle_y*15 && (this.walls_left[i] === this.circle_x*15 || this.walls_left[i]+15 === this.circle_x*15)){
+                        indentification = 1;
+                    }
+                }
 
-            for(let i = 0; i < this.walls.length; i++ ){
-                if(this.walls_top[i]-15 === this.circle_y*15 && (this.walls_left[i] === this.circle_x*15 || this.walls_left[i]+15 === this.circle_x*15)) {
-                  indentification = 1;
+                if(indentification !== 1){
+                    this.circle_y --;
                 }
             }
+        },
 
-          if(indentification !== 1){
-                this.circle_y ++;
-          }
+        right(){
+            if(this.circle_x < this.cell - 1){
+                let indentification = 0;
 
-      }
+                for(let i = 0; i < this.walls.length; i++ ){
+                    if((this.walls_top[i] === this.circle_y*15 || this.walls_top[i]+15 === this.circle_y*15) && this.walls_left[i]-15 === this.circle_x*15  ) {
+                        indentification = 1;
+                    }
+                }
+
+                if(indentification !== 1){
+                    this.circle_x ++;
+                }
+            }
+        },
+
+        bottom(){
+            if(this.circle_y < this.row -1){
+                let indentification = 0;
+
+                for(let i = 0; i < this.walls.length; i++ ){
+                    if(this.walls_top[i]-15 === this.circle_y*15 && (this.walls_left[i] === this.circle_x*15 || this.walls_left[i]+15 === this.circle_x*15)) {
+                        indentification = 1;
+                    }
+                }
+
+                if(indentification !== 1){
+                    this.circle_y ++;
+                }
+            }
+        },
+
+
+        run: function (e) {
+            switch (e.keyCode) {
+                case 37:
+                    this.left();
+                    break;
+                case 38:
+                    this.top();
+                    break;
+                case 39:
+                    this.right();
+                    break;
+                case 40:
+                    this.bottom();
+                    break;
+            }
+        }
     },
 
-
-    run: function (e) {
-        switch (e.keyCode) {
-            case 37:
-                this.left();
-                break;
-            case 38:
-                this.top();
-                break;
-            case 39:
-                this.right();
-                break;
-            case 40:
-                this.bottom();
-                break;
-        }
-    }
-
-
-  },
-
-
-  mounted(){
-    window.addEventListener('keydown', this.run);
-  },
-
-
-
+    mounted(){
+        window.addEventListener('keydown', this.run);
+    },
 
 });
 
