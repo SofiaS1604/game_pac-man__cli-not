38a2633f8eaef30d2c5a466289b3f8152c498b7f
SofiaS1604 <SofiaS1604@yandex.ru>
2019-07-01 02:27:35 +0300

    Add files via upload

--- site/js/script.js
+++ site/js/script.js
@@ -1,40 +1,9 @@
-if (document.getElementsByClassName('profile')[0]){
-    document.getElementsByClassName('profile')[1].addEventListener('click', () => {
-        document.getElementsByClassName('profile')[1].classList.add('profile--active');
-        document.getElementsByClassName('profile')[0].classList.remove('profile--active');
-        document.getElementsByClassName('profile')[2].classList.remove('profile--active');
-        document.querySelector('.employee-informatics').style.display = 'none';
-        document.querySelector('.employee-linguistics').style.display = 'flex';
-        document.querySelector('.employee-maths').style.display = 'none';
-    });
-
-    document.getElementsByClassName('profile')[0].addEventListener('click', () => {
-        document.getElementsByClassName('profile')[0].classList.add('profile--active');
-        document.getElementsByClassName('profile')[1].classList.remove('profile--active');
-        document.getElementsByClassName('profile')[2].classList.remove('profile--active');
-        document.querySelector('.employee-informatics').style.display = 'none';
-        document.querySelector('.employee-linguistics').style.display = 'none';
-        document.querySelector('.employee-maths').style.display = 'flex';
-    });
-
-    document.getElementsByClassName('profile')[2].addEventListener('click', () => {
-        document.getElementsByClassName('profile')[2].classList.add('profile--active');
-        document.getElementsByClassName('profile')[0].classList.remove('profile--active');
-        document.getElementsByClassName('profile')[1].classList.remove('profile--active');
-        document.querySelector('.employee-informatics').style.display = 'flex';
-        document.querySelector('.employee-linguistics').style.display = 'none';
-        document.querySelector('.employee-maths').style.display = 'none';
-    });
-}
-
-
-
 $(document).ready(function () {
-    let new_id;
+    let newSection;
     $('.section__classes').delegate('a', 'click', function(e) {
         e.preventDefault();
-        if(new_id !== undefined){
-            document.getElementsByClassName('section__class')[new_id].classList.remove('section__class--active');
+        if(newSection !== undefined){
+            document.getElementsByClassName('section__class')[newSection].classList.remove('section__class--active');
         }
         for (let i = 0; i < document.querySelectorAll('.section__block').length; i++){
             document.getElementsByClassName('section__block')[i].style.display = 'none';
@@ -42,7 +11,23 @@ $(document).ready(function () {
         document.getElementsByClassName('section__block')[$(this).index() + 1].style.display = 'flex';
         document.getElementsByClassName('section__class')[$(this).index()].classList.add('section__class--active');
 
-        new_id = $(this).index();
+        newSection = $(this).index();
+    });
+
+
+    let newEmployees = 0;
+    $('.profiles').delegate('a', 'click', function(e) {
+        e.preventDefault();
+
+        document.getElementsByClassName('profile')[newEmployees].classList.remove('profile--active');
+
+        for (let i = 0; i < document.querySelectorAll('.employees_container').length; i++){
+            document.getElementsByClassName('employees_container')[i].style.display = 'none';
+        }
+        document.getElementsByClassName('employees_container')[$(this).index()].style.display = 'flex';
+        document.getElementsByClassName('profile')[$(this).index()].classList.add('profile--active');
+
+        newEmployees = $(this).index();
     });
 
 });
